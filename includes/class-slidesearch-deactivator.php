47852d486f284a5c45656bc7ce9aae51f6a354c6
Mobeen Abdullah <mobeenabdullah@gmail.com>
2021-03-12 04:44:27 +0500

    added plugin initial files

---
+++ includes/class-slidesearch-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://mobeenabdullah.com
+ * @since      1.0.0
+ *
+ * @package    Slidesearch
+ * @subpackage Slidesearch/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Slidesearch
+ * @subpackage Slidesearch/includes
+ * @author     Mobeen Abdullah <mobeenabdullah@gmail.com>
+ */
+class Slidesearch_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
